cd3df315286f761ef45559d7cf30fe55208a61bb
Diana Scharf <scharf.diana@googlemail.com>
2023-07-25 17:52:58 +0200

    make typescript happy

--- api/_lib/sanitizer.ts
+++ api/_lib/sanitizer.ts
@@ -10,6 +10,6 @@ const entityMap: { [key: string]: string } = {
 export function sanitizeHtml(html: string) {
     return String(html)
       .replace(/[&<>"'\/]/g, (key) => entityMap[key])
-      .replaceAll("+", " ");
+      .replace(/\+/g, " ");
 }
 
